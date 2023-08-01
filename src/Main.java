a1988b7b58c6e12ef6da0b033e37690d8d00378e
Mak7im <mak7im2003@mail.ru>
2023-08-02 00:01:09 +0300

    Необходимо реализовать следующий метод: 1. Получаем на входе массив чисел. 2. Все четные числа увеличиваем на единицу. 3. Возвращаем кусок списка с 3-го по 7-й элемент.

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int[] weights = {1, 6, 2, 3, 7, 9, 22, 13};
+        System.out.println("Увеличиваем чётные числа на 1");
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] % 2 == 0) {
+                weights[i]++;
+            }
+            System.out.println(weights[i]);
+        }
+        System.out.println("Вывод с 3 по 7 число");
+        sumWeights(weights);
+    }
+
+
+    public static int sumWeights(int[] weights) {
+        for (int i = 3; i <= 7; i++) {
+            if (i < weights.length);
+            System.out.println(weights[i]);
+
+        }
+
+        return weights.length;
+
+    }
+}
