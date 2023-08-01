d6f9a6508579ebb3666c71738b691b922b7502b7
Mak7im <mak7im2003@mail.ru>
2023-08-02 00:06:33 +0300

    Билет № 5

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         int[] weights = {1, 6, 2, 3, 7, 9, 22, 13};
-        System.out.println("Увеличиваем чётные числа на 1");
+        System.out.println("Увеличиваем чётные числа на +1");
         for (int i = 0; i < weights.length; i++) {
             if (weights[i] % 2 == 0) {
                 weights[i]++;
             }
             System.out.println(weights[i]);
         }
-        System.out.println("Вывод с 3 по 7 число");
+        System.out.println("Вывод чисел с 3 по 7");
         sumWeights(weights);
     }
 
